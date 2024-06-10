a553f20fb70517b3fb08cd453ea9c61f62fa2a16
Lampdust <lampdust06@gmail.com>
2024-06-10 19:37:30 +0300

    last upd

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
 
-export const addOneContact = async () => {};
+export const addOneContact = async (number) => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contacts = JSON.parse(data);
 
-addOneContact();
+        const newContact = Array.from({ length: number }, createFakeContact);
+
+        const updatedContacts = [...contacts, newContact];
+
+        await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), 'utf-8');
+
+        console.log(`One contact generated and added to the file.`);
+    } catch (error) {
+        console.error('Error generating contact:', error);
+    }
+};
+
+addOneContact(1);
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -5,17 +5,13 @@ import fs from 'fs/promises';
 
 const generateContacts = async (number) => {
     try {
-        // Читаем существующие контакты из файла
         const data = await fs.readFile(PATH_DB, 'utf-8');
         const contacts = JSON.parse(data);
 
-        // Генерируем новые контакты
         const newContacts = Array.from({ length: number }, createFakeContact);
 
-        // Добавляем новые контакты к существующим
         const updatedContacts = [...contacts, ...newContacts];
 
-        // Записываем обновленные контакты обратно в файл
         await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2), 'utf-8');
 
         console.log(`${number} contacts generated and added to the file.`);
@@ -24,5 +20,4 @@ const generateContacts = async (number) => {
     }
 };
 
-// Получаем количество контактов из аргументов командной строки
 generateContacts(5);
